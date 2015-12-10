4708d89996854e806f0cc8281436a3dee9fc63c6
Damon Snyder <drsnyder@gmail.com>
2015-12-09 16:56:24 -0800

    Add state, update-state, add test for m&m prob

--- src/think_bayes/suite.clj
+++ src/think_bayes/suite.clj
@@ -7,19 +7,19 @@
   (probability [this hypothesis])
   (probabilities [this]))
 
-(defrecord SuiteRecord [hypotheses likelihood]
+(defrecord SuiteRecord [hypotheses likelihood state update-state]
   SuiteProtocol
   (update-hypothesis [this hypothesis]
     (let [likelihood (:likelihood this)
           updated (into {}
                         (for [[hypo prob] (:hypotheses this)]
-                          [hypo (* prob (likelihood hypothesis hypo))]))]
+                          [hypo (* prob (likelihood (:state this) hypothesis hypo))]))]
       (assoc this :hypotheses (hist/normalize-pmf updated))))
   (probability [this hypothesis]
     (get (:hypotheses this) hypothesis))
   (probabilities [this]
     (:hypotheses this)))
 
-(defn new-suite [hypotheses &{:keys [likelihood]}]
-  (->SuiteRecord (hist/pmf hypotheses) likelihood))
+(defn new-suite [hypotheses &{:keys [likelihood state update-state]}]
+  (->SuiteRecord (hist/pmf hypotheses) likelihood state update-state))
 
--- test/think_bayes/suite_test.clj
+++ test/think_bayes/suite_test.clj
@@ -4,12 +4,41 @@
 
 (facts :monty
   (let [monty (suite/new-suite ["A" "B" "C"]
-                               :likelihood (fn [data hypo]
+                               :likelihood (fn [_ selection hypo]
                                              (condp = hypo
-                                               data 0
+                                               selection 0
                                                "A"  0.5
                                                1)))
         updated-monty (suite/update-hypothesis monty "B")]
         (suite/probability updated-monty "B") => (roughly 0.0)
         (suite/probability updated-monty "A") => (roughly 0.33333)
         (suite/probability updated-monty "C") => (roughly 0.66666)))
+
+(facts :m_and_m
+  (let [mix94 {:brown 30
+               :yellow 20
+               :red 20
+               :green 10
+               :orange 10
+               :tan 10}
+        mix96 {:blue 24
+               :green 20
+               :orange 16
+               :yellow 14
+               :red 13
+               :brown 13
+               }
+        hypoA {:bag1 mix94
+               :bag2 mix96}
+        hypoB {:bag1 mix96
+               :bag2 mix94}
+        hypotheses {"A" hypoA "B" hypoB}
+        likelihood (fn [bowls selection hypo]
+                     (let [[bag color] selection
+                           like (get-in bowls [hypo bag color])]
+                       like))
+        mandm (suite/new-suite ["A" "B"] :likelihood likelihood :state hypotheses)
+        updated-mandm (suite/update-hypothesis mandm [:bag1 :yellow])
+        updated-mandm (suite/update-hypothesis updated-mandm [:bag2 :green])]
+    (suite/probability updated-mandm "A") => (roughly 0.74074)
+    (suite/probability updated-mandm "B") => (roughly 0.25925)))
